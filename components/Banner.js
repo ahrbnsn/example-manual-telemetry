441832d215fdccda50569606880d356be0405673
Ashley Robinson <github@rbnsn.xyz>
2022-02-17 13:38:10 -0500

    add banner component

---
+++ components/Banner.js
@@ -0,0 +1,12 @@
+import Link from "./Link";
+export function Banner() {
+  return (
+    <Link href="https://docs.honeycomb.io/getting-started/quickstart/">
+      <div className="signUpBanner">
+        Create a free account to explore your data in Honeycomb
+      </div>
+    </Link>
+  );
+}
+
+export default Banner;
